8c175cc1e648b29f995ee63f091299241a960af4
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2016-06-08 20:27:39 +0530

    [Assignment 3] Vidhi/Abhinav - Making API Call to fetch sessions

---
+++ app/src/test/java/com/thoughtworks/conference/presenter/AgendaPresenterTest.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.conference.presenter;
+
+import com.thoughtworks.conference.apiclient.APIClient;
+import com.thoughtworks.conference.apiclient.APIClientCallback;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class AgendaPresenterTest {
+
+  private APIClient mockApiClient;
+  private AgendaPresenter agendaPresenter;
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    mockApiClient = mock(APIClient.class);
+    agendaPresenter = new AgendaPresenter(mockApiClient);
+  }
+
+  @Test
+  public void shouldCallApiClient() {
+    agendaPresenter.fetchEvents();
+    verify(mockApiClient, times(1)).get(eq("https://intense-fire-9666.firebaseio.com/"), any(APIClientCallback.class));
+  }
+
+}
