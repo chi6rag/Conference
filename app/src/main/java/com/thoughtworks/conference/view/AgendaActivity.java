6cf2f2005712f2e783e763105e29a1d1b85e0ffc
NazneenRupawalla <nazneenr@thoughtworks.com>
2016-06-11 18:01:30 +0530

    Showing a dialog on no network connection.

--- app/src/main/java/com/thoughtworks/conference/view/AgendaActivity.java
+++ app/src/main/java/com/thoughtworks/conference/view/AgendaActivity.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.conference.view;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
@@ -47,8 +49,18 @@ public class AgendaActivity extends AppCompatActivity implements AgendaView {
   }
 
   @Override
-  public void showErrorDialog(String eq) {
-
+  public void showErrorDialog(String message) {
+    AlertDialog alertDialog = new AlertDialog.Builder(this)
+        .setMessage(message)
+        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+            dialogInterface.dismiss();
+            finish();
+          }
+        })
+        .create();
+    alertDialog.show();
   }
 
   @Override
